707581e9268768bc3458810a37253ab01d384f72
Greg Ladden <greg@gl-digital.com>
2019-05-07 23:17:03 -0500

    completed logic and clean up code

--- logic.js
+++ logic.js
@@ -1,23 +1,47 @@
 
-function showWeather(zipCode, countryName) {
-  const key = '638f24bfdcba6d2e495a079fe6fd882f'
-  fetch('http://api.openweathermap.org/data/2.5/weather?zip=' + zipCode + ',' + countryName + '&APPID=' + key)
-    .then(resp => { return resp.json() }) // Convert data to json
-    .then(data => {
-      console.log(data);
-      handlers(data)
+
+
+const currentWeather = {
+
+  showWeather: function () {
+    const testButton = document.getElementById('testButton');
+    testButton.addEventListener('click', function (zipCode, country) {
+      const searchZipTextInput = document.getElementById('searchZipTextInput');
+      const searchCountryTextInput = document.getElementById('searchCountryTextInput')
+      zipCode = parseInt(searchZipTextInput.value);
+      country = searchCountryTextInput.value
+
+      const key = '638f24bfdcba6d2e495a079fe6fd882f'
+      fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${zipCode},${country}&APPID=${key}`)
+        .then(resp => { return resp.json() }) // Convert data to json
+        .then(data => {
+          console.log(data);
+          currentWeather.displayData(data)
+          searchCountryTextInput.value = ''
+          searchZipTextInput.value = ''
+        })
+        .catch(function (error) {
+          // catch any errors
+          console.log(error)
+        });
     })
-    .catch(function () {
-      // catch any errors
-    });
-}
+  },
+  displayData: function (data) {
 
-function handlers(data) {
-  const fahrenheit = Math.round(((parseFloat(d.main.temp) - 273.15) * 1.
-  document.getElementById('cityName').innerHTML = data.name
-  document.getElementById('cityName').innerHTML = data.name
-}
+    if (data.cod === '404') {
+      document.getElementById('cityName').innerHTML = `${data.message}. Please try again`;
+    } else {
+      const currentTemp = Math.round(((parseFloat(data.main.temp) - 273.15) * 1.8) + 32);
+      const todayHi = Math.round(((parseFloat(data.main.temp_max) - 273.15) * 1.8) + 32);
+      const todayLow = Math.round(((parseFloat(data.main.temp_min) - 273.15) * 1.8) + 32);
+      const currentHumidity = data.main.humidity;
 
-window.onload = function () {
-  showWeather('78731', 'US');
+      document.getElementById('cityName').innerHTML = data.name;
+      document.getElementById('temp').innerHTML = `${currentTemp} &deg`;;
+      document.getElementById('todayHi').innerHTML = `${todayHi} &deg`;;
+      document.getElementById('todayLow').innerHTML = `${todayLow} &deg`;;
+      document.getElementById('currentHumidity').innerHTML = `${currentHumidity}%`;
+    }
+  }
 }
+currentWeather.showWeather()
